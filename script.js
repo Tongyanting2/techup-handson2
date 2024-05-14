f67965f52a550b8073f43635059e3efa9f4d99db
Tongyanting2 <tongyanting.beryl@gmail.com>
2024-05-14 04:29:56 +0000

    completed hands on 3

---
+++ script.js
@@ -0,0 +1,31 @@
+var age = 20
+age = 65
+console.log (age)
+var username = "yanting"
+var password = "techup"
+
+var isMale = true
+var isLoggedIn= false
+
+var blog_posts =["Today was a good day" , "The sky is blue", "It's raining heavily now"]
+
+console.log (age)
+console.log(username);
+console.log (password)
+console.log (isMale)
+console.log (isLoggedIn)
+console.log (blog_posts)
+
+if (isMale==true){
+console.log ("You are Male")
+}else{
+console.log ("You are not make");
+}
+
+while (age<70){
+console.log ("Your age is "+age);
+age = age +1;
+}
+
+function sayHello (){
+alert ("hello everyone")
